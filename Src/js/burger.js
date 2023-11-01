37fb129994792c21764984d9e42ca5155673fc53
Jesus Santiago <jesusantiago420@gmail.com>
2023-11-01 15:18:48 -0300

    First commit - version mobile complet

---
+++ Src/js/burger.js
@@ -0,0 +1,8 @@
+const fatBurger = document.getElementById("fat-burger")
+const burger = document.getElementById("btn-burger")
+
+
+burger.addEventListener("click", ()=> {
+        fatBurger.classList.toggle("close-burger")
+    }
+)
