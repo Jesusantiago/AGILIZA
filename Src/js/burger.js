a7bd5ce61af5065a69befbd48b79eff04557576e
Jesus Santiago <jesusantiago420@gmail.com>
2023-11-13 21:52:19 -0300

    [CHANGE] style (mobile - desktop)

--- Src/js/burger.js
+++ Src/js/burger.js
@@ -6,3 +6,32 @@ burger.addEventListener("click", ()=> {
         fatBurger.classList.toggle("close-burger")
     }
 )
+
+const about = document.querySelector("swiper-container");
+const mision = document.getElementsByClassName("mision")
+const vision = document.getElementsByClassName("vision")
+const valores = document.getElementsByClassName("valores");
+
+// console.log(about.firstElementChild.attributes.class)
+
+// setInterval(() => {console.log(about.firstElementChild)},0.500)
+// about.addEventListener("change", (event) =>{
+    
+// })
+
+// setTimeout(function(){
+//     console.log("Hola Mundo");
+// }, 2000);
+
+// console.log("setTimeout() Ejemplo...");
+
+
+about.addEventListener("click", () => {
+    if(about.firstElementChild.attributes.class == "about_div mision"){
+        console.log("hola desde mision")
+    }else if(about.firstElementChild.attributes.class == "about_div vision" ){
+        console.log("hola desde vision")
+    }else{
+        console.log("valores")
+    }
+})
